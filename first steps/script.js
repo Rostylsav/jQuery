b7793ca84d7628baf61f41eeeae01749b38e440e
Rostyk <pas.ros.bor@gmail.com>
2014-04-14 15:33:35 +0300

    1.1

--- first steps/script.js
+++ first steps/script.js
@@ -1,16 +1,86 @@
 (function($, undefined){
 
-    function createDiv (obj){
-        var myDiv = $('<div>').attr({
-            'id': obj.id,
-            'class': 'div',
-            'text': obj.text
+    var collection = [{id:2, task : 'Rostyk', status:false},{id:2, task : 'Rostyk', status:false},{id:2, task : 'Rostyk', status:false},{id:2, task : 'Rostyk', status:false}],
+        idTask=0; 
+    function enterTask(event)
+    {
+        if(event.keyCode === 13 )
+        {
+            idTask++;
+            var value = $('input').val();
+            showTask({id: idTask, task : value, status : false});
+            collection.push({id: idTask, task : value, status : false});
+            $('input').val('');
+
+        }
+    }
+    function createInput (){
+        var div = $('<div>').attr({
+            'id': 'enterTask',
+            'class': 'enterTaskDiv'
         });
 
-        $('#container').append(myDiv);
+        var input = $('<input>').attr({
+            'id': 'input',
+            'class': 'input',
+            'placeholder': 'What needs to be done?'
+        });
+        var listTasks = $('<div>').attr({
+            'id': 'listTasks'
+        });
+        input.on('keydown', enterTask);
+        div.append(input);
+        $('#wrapper').append(div);
+        $('#wrapper').append(listTasks);
+        
     };
 
+    function showTask(obj){
+        var container = $('<div>').attr({
+            'id' : 'oneTask' + obj.id,
+            'class': 'oneTask'
+        });
+
+        var checkbox = $('<input>').attr({
+            'id': 'checkbox' + obj.id,
+            'class': 'checkbox',
+            'type' : 'checkbox',
+            'checked' : obj.status
+        });
+
+        var div = $('<div>').attr({
+            'id' : 'task' + obj.id,
+            'class': 'task'
+        });
+
+        div.text(obj.task);
+
+        var button = $('<button>').attr({
+            'id' : 'button' + obj.id,
+            'class': 'button'
+        });
+        button.text('R');
+
+
+        container.append(checkbox);
+        container.append(div);
+        container.append(button);
+
+        $('#listTasks').append(container);
+
+    }
+
+    function showAll(array)
+    {
+        $('#listTasks').text('');
+        for(var i = 0; i < array.length; i++)
+        {
+            showTask(array[i]);
+        }
+    }
     $(function(){
-        $("#container").click( createDiv({id:1, text: 'Rostyk'}) );
+       createInput();
+
+       showAll(collection);
     });
 })(jQuery); 
